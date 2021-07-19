28cf5abede350d5a981ea33e24587369785d3e27
Ivcota <ivcotad@gmail.com>
2021-07-19 13:07:42 -0700

    Sign and Return Tokens

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const { user } = require("../prisma/db");
 const bycrpt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//POST - Create User
 router.post("/", async (req, res) => {
   // Extract Sent Request
   const { name, email, password } = req.body;
@@ -26,6 +27,58 @@ router.post("/", async (req, res) => {
     expiresIn: "30d",
   });
 
+  // Send Token
+  res.json({
+    success: true,
+    token,
+  });
+});
+
+//POST - Login
+router.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validate Email and Password
+  if (!email || !password) {
+    res.status(400).json({
+      error: "Please enter username or password",
+    });
+  }
+
+  // Check for user
+  const storedUser = await user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: true,
+    },
+  });
+
+  if (!user) {
+    res.status(401).json({
+      error: "Invalid",
+    });
+  }
+
+  // Check Password
+  const isMatch = await bycrpt.compare(password, storedUser.password);
+  if (!isMatch) {
+    res.status(401).json({
+      error: "Invalid",
+    });
+  }
+
+  // Sign JWT and Return Token
+  const token = jwt.sign({ id: storedUser.id }, process.env.SECRET, {
+    // Expires in 30 Days
+    expiresIn: "30d",
+  });
+
+  // Send Token
   res.json({
     success: true,
     token,
